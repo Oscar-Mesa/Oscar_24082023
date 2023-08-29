06b029619f7375777cd623e9fb3332697e9494df
Oscar-Mesa <oscar.mesaqu@amigo.edu.co>
2023-08-29 08:06:18 -0500

    agrego color y botones al diseño

---
+++ App_Reto1/App_Reto1/frm_registro.aspx
@@ -0,0 +1,61 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="frm_registro.aspx.cs" Inherits="App_Reto1.frm_registro" %>
+
+<!DOCTYPE html>
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
+    <title>Formulario de Registro de Usuarios</title>
+    <link rel="stylesheet" type="text/css" href="estilos/estilos.css"/>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div id="container">
+            <h3>Formulario de Registro de Crédito</h3>
+            <div class="form-group">
+                <asp:TextBox ID="txt_codigo" runat="server" placeholder="ID *" required ="true"></asp:TextBox> 
+            </div>
+            <div class="form-group">
+                <asp:TextBox ID="txt_nombre" runat="server" placeholder="Nombres *"></asp:TextBox> 
+            </div>
+            <div class="form-group">
+                <asp:TextBox ID="TextBox1" runat="server" placeholder="Apellidos *" ></asp:TextBox> 
+            </div>
+            <div class="form-group">
+                <asp:TextBox ID="txt_contacto" runat="server" placeholder="Contacto *" ></asp:TextBox> 
+            </div>
+            <div class="form-group">
+                <asp:TextBox ID="txt_correo" runat="server" placeholder="Correo *" ></asp:TextBox> 
+            </div>
+            <div class="form-group">
+                <asp:TextBox ID="txt_direccion" runat="server" placeholder="Dirección *" ></asp:TextBox> 
+            </div>
+            <div class="form-group">
+                <asp:TextBox ID="txt_salario" runat="server" placeholder="Salario *" ></asp:TextBox> 
+            </div>
+            <div class="form-group">
+                <asp:TextBox ID="txt_empresa_labora" runat="server" placeholder="Empresa en que trabaja *" ></asp:TextBox> 
+            </div>
+            <div class="form-group">
+                <asp:TextBox ID="txt_estado_credito" runat="server" placeholder="Estado del credito *" ></asp:TextBox> 
+            </div>
+            <div class="form-group">
+                <asp:TextBox ID="txt_fecha_solicitud" runat="server" placeholder="Fecha de la solicitud *" ></asp:TextBox> 
+            </div>
+
+            <asp:DropDownList ID="cbx_plazo" runat="server" CssClass="cbx">
+                <asp:ListItem Value="6 ">6 Meses</asp:ListItem>
+                <asp:ListItem Value="12 ">12 meses</asp:ListItem>
+                <asp:ListItem Value="24 ">24 meses</asp:ListItem>
+                <asp:ListItem Value="36 ">36 meses</asp:ListItem>
+            </asp:DropDownList>
+
+            <div class="btn-group">
+                <asp:Button ID="btn_registrar" runat="server" Text="Registrar" CssClass="btn"  />
+                <asp:Button ID="btn_cancelar" runat="server" Text="Cancelar" CssClass="btn btn-cancelar"  />
+                <asp:Button ID="btn_consultar" runat="server" Text="Consulta" CssClass="btn" />
+            </div>
+            <asp:Label ID="lbl_mensaje" runat="server" Text=""></asp:Label>
+        </div>
+    </form>
+</body>
+</html>
---
+++ App_Reto1/App_Reto1/frm_registro.aspx.designer.cs
@@ -0,0 +1,161 @@
+﻿//------------------------------------------------------------------------------
+// <generado automáticamente>
+//     Este código fue generado por una herramienta.
+//
+//     Los cambios en este archivo podrían causar un comportamiento incorrecto y se perderán si
+//     se vuelve a generar el código. 
+// </generado automáticamente>
+//------------------------------------------------------------------------------
+
+namespace App_Reto1
+{
+
+
+    public partial class frm_registro
+    {
+
+        /// <summary>
+        /// Control form1.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.HtmlControls.HtmlForm form1;
+
+        /// <summary>
+        /// Control txt_codigo.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox txt_codigo;
+
+        /// <summary>
+        /// Control txt_nombre.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox txt_nombre;
+
+        /// <summary>
+        /// Control TextBox1.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox TextBox1;
+
+        /// <summary>
+        /// Control txt_contacto.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox txt_contacto;
+
+        /// <summary>
+        /// Control txt_correo.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox txt_correo;
+
+        /// <summary>
+        /// Control txt_direccion.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox txt_direccion;
+
+        /// <summary>
+        /// Control txt_salario.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox txt_salario;
+
+        /// <summary>
+        /// Control txt_empresa_labora.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox txt_empresa_labora;
+
+        /// <summary>
+        /// Control txt_estado_credito.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox txt_estado_credito;
+
+        /// <summary>
+        /// Control txt_fecha_solicitud.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox txt_fecha_solicitud;
+
+        /// <summary>
+        /// Control cbx_plazo.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.DropDownList cbx_plazo;
+
+        /// <summary>
+        /// Control btn_registrar.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Button btn_registrar;
+
+        /// <summary>
+        /// Control btn_cancelar.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Button btn_cancelar;
+
+        /// <summary>
+        /// Control btn_consultar.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Button btn_consultar;
+
+        /// <summary>
+        /// Control lbl_mensaje.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label lbl_mensaje;
+    }
+}
